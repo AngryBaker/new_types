47cf99dfeb937f7524347c6f51c44fceb2cc8b7e
Angry Baker <checheninlive@mail.ru>
2024-09-05 20:15:02 +0300

    minor update

--- src/ts/__tests__/Movie.test.ts
+++ src/ts/__tests__/Movie.test.ts
@@ -1,9 +1,16 @@
 import Cart from '../service/Cart';
 import Movie from '../domain/Movie';
 
-test('new card should be empty', () => {
+test('new card shouldnt be empty', () => {
   const cart = new Cart();
-  const film = new Movie(666, "The Electrical Life of Louis Wain", 2021, "UK", "You make the world beautiful and warm and kind", "drama", 25, 111);
+  const film = new Movie(666,
+    "The Electrical Life of Louis Wain",
+    2021,
+    "UK",
+    "You make the world beautiful and warm and kind",
+    "drama",
+    25,
+    111);
   cart.add(film);
 
   expect(cart.items.length).toBe(1);
